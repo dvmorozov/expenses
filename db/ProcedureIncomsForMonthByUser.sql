89e6be9035808a0c579e855e2e57807048d0be5e
Dmitry Morozov <dvmorozov@hotmail.com>
2017-04-06 20:32:10 +0200

    Now notes are displayed at the month income page.
    https://action.mindjet.com/task/14665340

--- db/ProcedureIncomsForMonthByUser.sql
+++ db/ProcedureIncomsForMonthByUser.sql
@@ -34,5 +34,19 @@ BEGIN
 	ORDER BY Date ASC
 END
 
+-- =============================================
+-- Author:		D.V.Morozov
+-- Create date: 06/04/2017
+-- Description:	https://action.mindjet.com/task/14665340
+-- =============================================
+CREATE PROCEDURE [dbo].[IncomsForMonthByUser3] @Year int, @Month int, @DataOwner UNIQUEIDENTIFIER 
+AS 
+BEGIN
+	SELECT ID, Date, Cost AS Amount, Name, EncryptedName, Currency, Note
+	FROM Operations
+	WHERE DataOwner = @DataOwner AND Income IS NOT NULL AND Income = 1 AND DATEPART(YEAR, DATE) = @YEAR AND DATEPART(MONTH, DATE) = @MONTH 
+	ORDER BY Date ASC
+END
+
 GO
 
--- www/ExpensesAzure/Models/Expenses.Designer.cs
+++ www/ExpensesAzure/Models/Expenses.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "E:\04 - morozov\my\appsoft\expenses\www\azure\ExpensesAzure\ExpensesAzure\Models\Expenses.edmx" включено. 
+﻿// Создание кода T4 для модели "E:\04 - github repos\expenses\www\ExpensesAzure\Models\Expenses.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
