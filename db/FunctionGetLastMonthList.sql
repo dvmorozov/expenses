7a4e190f8b2fa035a9b72151f8dd5b41a19594a2
Dmitry Morozov <dvmorozov@hotmail.com>
2018-07-22 13:47:04 +0200

    Table function GetLastMonthListWithCurrencies was added.
    #17

--- db/FunctionGetLastMonthList.sql
+++ db/FunctionGetLastMonthList.sql
@@ -9,12 +9,12 @@ GO
 DROP FUNCTION expenses.GetLastMonthList
 GO
 
--- =============================================
+-- ==========================================================================================
 -- Author:		Dmitry Morozov
 -- Create date: 21/07/2018
--- Description:	Returns list of last months/years of 
---				given length starting from current date.
--- =============================================
+-- Description:	Returns list of last months/years of given length starting from current date.
+-- ==========================================================================================
+
 CREATE FUNCTION expenses.GetLastMonthList
 (
 	@LastMonthNumber INT
@@ -35,6 +35,43 @@ BEGIN
 		VALUES (DATEPART(month, @Now), DATEPART(year, @Now))
 		SET @Now = DATEADD(mm, -1, @Now)
 	END
+	RETURN
+END
+GO
+
+DROP FUNCTION expenses.GetLastMonthListWithCurrencies
+GO
+
+-- ==========================================================================================
+-- Author:		Dmitry Morozov
+-- Create date:	22/07/2018
+-- Description:	Returns list of last months/years of date given length starting from current 
+--				joined with list of all currencies used by given user.
+-- ==========================================================================================
+
+CREATE FUNCTION expenses.GetLastMonthListWithCurrencies
+(
+	@LastMonthNumber INT,
+	@DataOwner UNIQUEIDENTIFIER
+)
+RETURNS 
+@ResuLt TABLE 
+(
+	Month INT,
+	Year INT,
+	Currency NCHAR(5)
+)
+AS
+BEGIN
+	INSERT INTO @Result
+		SELECT *
+		FROM [expenses].GetLastMonthList(@LastMonthNumber)
+		CROSS JOIN
+		(
+			SELECT DISTINCT Currency
+			FROM [expenses].Expenses
+			WHERE DataOwner = @DataOwner
+		) C
 	RETURN
 END
 GO
