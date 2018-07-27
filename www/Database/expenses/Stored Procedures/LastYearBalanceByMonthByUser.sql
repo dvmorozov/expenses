3e89be43d9ee760e6acfa4f5e1523c8bd86584b0
Dmitry Morozov <dvmorozov@hotmail.com>
2018-07-27 09:59:43 +0200

    Bug in stored procedure LastYearBalanceByMonthByUser was fixed. Unit test was added.
    #28

--- www/Database/expenses/Stored Procedures/LastYearBalanceByMonthByUser.sql
+++ www/Database/expenses/Stored Procedures/LastYearBalanceByMonthByUser.sql
@@ -1,4 +1,8 @@
-﻿-- =============================================
+﻿
+DROP PROCEDURE [expenses].LastYearBalanceByMonthByUser
+GO
+
+-- =============================================
 -- Author:		D.V.Morozov
 -- Create date: 08/06/2015
 -- Description:	https://www.evernote.com/shard/s132/nl/14501366/47e64199-5c58-43a1-9d0d-9d3081811def
@@ -6,11 +10,16 @@
 CREATE PROCEDURE [expenses].LastYearBalanceByMonthByUser @LastMonthNumber INT, @DataOwner UNIQUEIDENTIFIER
 AS
 BEGIN
-	SELECT TOP (@LastMonthNumber) t.Y, t.M, 
-		COALESCE(m.Income, 0)- t.Total AS Balance
-	--  https://github.com/dvmorozov/expenses/issues/17
-	FROM expenses.GetLastYearTotalExpensesByMonthByUser(@LastMonthNumber, @DataOwner) t
+	--	For compatibility just the first subset of data is selected.
+	--	Set returned by the function already ordered properly, don't use ORDER.
+	-- https://github.com/dvmorozov/expenses/issues/28
+	SELECT t.Y, t.M, COALESCE(m.Income, 0)- t.Total AS Balance
+	FROM 
+	(
+		SELECT TOP (@LastMonthNumber) *
+		--	https://github.com/dvmorozov/expenses/issues/17
+		FROM expenses.GetLastYearTotalExpensesByMonthByUser(@LastMonthNumber, @DataOwner) t
+	) t
 	LEFT JOIN Month m
-	ON t.Y = m.Year AND t.M = m.Month
-	ORDER BY t.Y, t.M
+	ON t.Y = m.Year AND t.M = m.Month AND t.Currency = m.Currency
 END
