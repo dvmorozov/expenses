988460c418d6cd6ce8a6c44568ee2259ef23c1c2
Dmitry Morozov <dvmorozov@hotmail.com>
2018-08-02 09:44:29 +0200

    Model was updated.
    #38

---
+++ www/Database/expenses/Stored Procedures/EstimatedCategoriesByUser4.sql
@@ -0,0 +1,20 @@
+﻿
+-- ==========================================================================================
+-- Author:		D.V.Morozov
+-- Create date: 02/08/2018
+-- Description:	https://github.com/dvmorozov/expenses/issues/38
+-- ==========================================================================================
+
+CREATE PROCEDURE [expenses].EstimatedCategoriesByUser4 @Year INT, @Month INT, @Day INT, @DataOwner UNIQUEIDENTIFIER, @ShortList BIT
+AS 
+BEGIN
+	SELECT
+		Name AS NAME,
+		Limit,
+		CategoryID AS ID,
+		CategoryTotal AS Total,
+		Estimation,
+		EncryptedName,
+		Currency
+	FROM [expenses].GetEstimatedCategoriesByUser(@Year, @Month, @Day, @DataOwner, @ShortList)
+END
