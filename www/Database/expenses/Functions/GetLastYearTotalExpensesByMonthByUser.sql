ac61bbcd00c922226ccaab15922e85eef75b2957
Dmitry Morozov <dvmorozov@hotmail.com>
2018-08-14 08:46:51 +0200

    Table-valued function GetLastYearTotalExpensesByMonthByUser was refactored to reuse function GetLastYearCategoryExpensesByMonthByUser.
    #54

--- www/Database/expenses/Functions/GetLastYearTotalExpensesByMonthByUser.sql
+++ www/Database/expenses/Functions/GetLastYearTotalExpensesByMonthByUser.sql
@@ -8,7 +8,7 @@
 --				All parts must have the same size equal to @LastMonthNumber, the dataset
 --				must be ordered by currency at first.
 -- =============================================
-CREATE FUNCTION expenses.GetLastYearTotalExpensesByMonthByUser
+CREATE FUNCTION [expenses].[GetLastYearTotalExpensesByMonthByUser]
 (
 	@LastMonthNumber INT, 
 	@DataOwner UNIQUEIDENTIFIER
@@ -25,65 +25,9 @@ RETURNS
 )
 AS
 BEGIN
-	--	https://www.evernote.com/shard/s132/nl/14501366/67b5959f-63bc-4cd5-af1a-a481a2859c50
-	INSERT INTO @Result 
-	SELECT
-		Y,
-		M,
-		COALESCE(SingleTotal, 0) + COALESCE(MonthlyTotal, 0) AS Total,
-		CAST(Y AS VARCHAR) + '/' + 
-		CASE WHEN LEN(CAST(M AS VARCHAR)) < 2 THEN '0' + CAST(M AS VARCHAR)
-		ELSE CAST(M AS VARCHAR) END AS Month, Currency
-	FROM
-	(
-		SELECT 
-			COALESCE(E1.Y, E2.Y) AS Y,
-			COALESCE(E1.M, E2.M) AS M,
-			COALESCE(SingleTotal, 0) AS SingleTotal,
-			COALESCE(MonthlyTotal, 0) AS MonthlyTotal,
-			COALESCE(E1.Currency, E2.Currency) AS Currency
-		FROM
-		(
-			-- Selects single-time expenses.
-			SELECT 
-				Year AS Y, Month AS M, 
-				E1.Currency, 
-				SingleTotal
-			FROM [expenses].GetLastMonthListWithCurrencies(@LastMonthNumber, @DataOwner, DEFAULT) E1
-			LEFT OUTER JOIN
-			(
-				SELECT 
-					SUM(Cost) AS SingleTotal, 
-					Currency, 
-					DATEPART(year, Date) AS Y,
-					DATEPART(month, Date) AS M
-				FROM [expenses].Expenses
-				WHERE DataOwner = @DataOwner AND (Monthly IS NULL OR Monthly = 0)
-				GROUP BY DATEPART(year, Date), DATEPART(month, Date), Currency
-			) E2
-			ON E1.Year = E2.Y 
-				AND E1.Month = E2.M
-				AND E1.Currency = E2.Currency
-		) E1
-		FULL OUTER JOIN
-		(
-			--	Selects montly expenses.
-			SELECT Year AS Y, Month AS M, SUM(Cost) AS MonthlyTotal, E1.Currency
-			FROM [expenses].GetLastMonthListWithCurrencies(@LastMonthNumber, @DataOwner, DEFAULT) E1
-			LEFT JOIN
-			(
-				SELECT Cost, FirstMonth, LastMonth, Currency
-				FROM [expenses].Expenses
-				WHERE DataOwner = @DataOwner AND Monthly IS NOT NULL AND Monthly = 1
-			) E2
-			ON DATEFROMPARTS(Year, Month, 1) >= E2.FirstMonth 
-				AND (DATEFROMPARTS(Year, Month, 1) <= E2.LastMonth OR E2.LastMonth IS NULL) 
-				AND (E1.Currency = E2.Currency)
-			GROUP BY Year, Month, E1.Currency
-		) E2
-		ON E1.Y = E2.Y AND E1.M = E2.M AND (E1.Currency = E2.Currency OR E1.Currency IS NULL AND E2.Currency IS NULL)
-	)
-	AS T
-	ORDER BY Currency, Y, M
+	--  https://github.com/dvmorozov/expenses/issues/54
+	INSERT INTO @Result
+	SELECT Y, M, Total, Month, Currency
+	FROM [expenses].[GetLastYearCategoryExpensesByMonthByUser](@LastMonthNumber, @DataOwner, NULL)
 	RETURN 
 END
