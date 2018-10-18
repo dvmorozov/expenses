fb3cd50909df4256bcd034baaa0278f70dc41b05
Dmitry Morozov <dvmorozov@hotmail.com>
2018-10-18 21:08:44 +0200

    Controller handles the multiplier.
    #101

--- www/ExpensesAzure/Controllers/MobileController.cs
+++ www/ExpensesAzure/Controllers/MobileController.cs
@@ -643,7 +643,7 @@ namespace SocialApps.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddExpense(int day, int month, int year, int hour, int min, int sec, string cost, string currency, string note, short? rating, short? importance, string project)
+        public ActionResult AddExpense(int day, int month, int year, int hour, int min, int sec, string cost, string currency, string note, short? rating, short? importance, string project, int multiplier)
         {
             try
             {
@@ -676,7 +676,12 @@ namespace SocialApps.Controllers
 
                 Session["ClientExpenseDate"] = clientExpenseDate;
 
-                _repository.AddExpense(clientExpenseDate, expense.Name, amount, note, false, null, null, expense.EncryptedName, currency, rating, categoryId, importance, project, userId);
+                //  https://github.com/dvmorozov/expenses/issues/101
+                if (multiplier > 0)
+                    amount = amount * multiplier;
+
+                _repository.AddExpense(clientExpenseDate, expense.Name, amount, note, false, null, null, 
+                    expense.EncryptedName, currency, rating, categoryId, importance, project, userId);
 
                 DropSessionLinks();
                 
