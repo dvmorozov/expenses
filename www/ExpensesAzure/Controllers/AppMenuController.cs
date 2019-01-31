fd3524d57c8ecccc90946e9b18a58dd0bd6d48b3
Dmitry Morozov <dvmorozov@hotmail.com>
2019-01-31 10:02:12 +0100

    New project AppMenu created. ErrorHandlingController moved to separate source file.
    #140

---
+++ www/ExpensesAzure/Controllers/AppMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SocialApps.Controllers
+{
+    [Authorize]
+    public partial class MobileController : Controller
+    {
+        //  https://www.evernote.com/shard/s132/nl/14501366/c707248c-3cab-47d7-838a-ec2b791e4ea7
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                return View("Error", new HandleErrorInfo(e, "Mobile", "Index"));
+            }
+        }
+    }
+}
